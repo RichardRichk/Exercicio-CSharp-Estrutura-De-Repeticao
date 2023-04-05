c41b122aedf8ebea7d437a8f37f9836de6b9db7b
Richard Fernandes Passarelli <45811737874@info.local>
2023-04-05 10:54:44 -0300

    Explicacao DoWhile e For

--- Program.cs
+++ Program.cs
@@ -6,59 +6,62 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+                //WHILE
 
-            //Coletar nome
-            //Validar nome != nulo
+            // Console.WriteLine($"Qual sua idade ?");
+            // idade = int.Parse(Console.ReadLine());
 
-            //Coletar idade
-            //Validar idadr = 0<100
+            // while (idade <0 && idade <=100)
+            // {
+            //     Console.WriteLine($"insira uma idade valida");
+            //     idade = int.Parse(Console.ReadLine()); 
+            // }
 
-            //coletar salario
-            //Validar salario >0
+            // Console.WriteLine($"Qual seu salario ?");
+            // salario = float.Parse(Console.ReadLine());
 
-            //coletar Estado civil (s = solteiro), (c = casado), (v = viuvo), (d = divorciado);
-
-            string nome;
-            int idade;
-            float salario;
-            char EstadoCivil;
-
-            Console.WriteLine($"Ola, qual seu nome ?");
-            nome = (Console.ReadLine().Trim(' '));
-
-            while (string.IsNullOrEmpty(nome))
-            {
-                Console.WriteLine($"Insira um nome valido");
-                nome = (Console.ReadLine().Trim(' '));
-            }
-
-            Console.WriteLine($"Qual sua idade ?");
-            idade = int.Parse(Console.ReadLine());
-
-            while (idade <0 && idade <=100)
-            {
-                Console.WriteLine($"insira uma idade valida");
-                idade = int.Parse(Console.ReadLine()); 
-            }
-
-            Console.WriteLine($"Qual seu salario ?");
-            salario = float.Parse(Console.ReadLine());
-
-            while (salario <0)
-            {
-                Console.WriteLine($"insira um salario valido");
-                salario = float.Parse(Console.ReadLine());
-            }
+            // while (salario <0)
+            // {
+            //     Console.WriteLine($"insira um salario valido");
+            //     salario = float.Parse(Console.ReadLine());
+            // }
             
-            Console.WriteLine($"Qual seu Estado-Civil ?\n(S = solteiro), (C = casado), (V = viuvo), (D = divorciado)");
-            EstadoCivil = char.Parse(Console.ReadLine().ToLower());
+            // Console.WriteLine($"Qual seu Estado-Civil ?\n(S = solteiro), (C = casado), (V = viuvo), (D = divorciado)");
+            // EstadoCivil = char.Parse(Console.ReadLine().ToLower());
 
-            while (EstadoCivil != 's' || EstadoCivil != 'c' || EstadoCivil != 'v' || EstadoCivil != 'd')
-            {
-                Console.WriteLine($"insira um Estado-Civil valido\n(S = solteiro), (C = casado), (V = viuvo), (D = divorciado)");
-                EstadoCivil = char.Parse(Console.ReadLine().ToLower());
-            }
+            // while (EstadoCivil != 's' || EstadoCivil != 'c' || EstadoCivil != 'v' || EstadoCivil != 'd')
+            // {
+            //     Console.WriteLine($"insira um Estado-Civil valido\n(S = solteiro), (C = casado), (V = viuvo), (D = divorciado)");
+            //     EstadoCivil = char.Parse(Console.ReadLine().ToLower());
+            // }
+
+                    //DO WHILE
             
+            // bool idadeCerta = false;
+            // int idade;
+
+            // Console.WriteLine($"Qual a idade do Richard ?");
+            // idade = int.Parse(Console.ReadLine()); 
+
+            // if (idade != 19)
+            // {
+            //      do
+            // {
+            //     Console.WriteLine($"Errou, e agora, Qual a idade do Richard ?");
+            //     idade = int.Parse(Console.ReadLine());    
+
+            // } while (idade != 19);
+            // }
+
+
+                    //FOR
+            
+            for (int t = 0; t <=50; t++)
+            {
+                Console.WriteLine(t);
+                
+            }
+
             
             
                 
